8d4f00935307ab40be6ed91b4bbe57639610276f
Frances <secnarfdelacruz@gmail.com>
2026-02-21 18:05:25 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            List<String> origin = new List<String>();
+
+            origin.Add("Manila");
+            origin.Add("Manila");
+            origin.Add("Bacolod");
+            origin.Add("Bohol");
+            origin.Add("Boracay");
+            origin.Add("Butuan");
+            origin.Add("Cagayan de Oro");
+            origin.Add("Calbayog");
+            origin.Add("Camiguin");
+            origin.Add("Cauayan");
+
+            List<String> desti = new List<String>();
+
+            desti.Add("Manila");
+            desti.Add("Bacolod");
+            desti.Add("Bohol");
+            desti.Add("Boracay");
+            desti.Add("Butuan");
+            desti.Add("Cagayan de Oro");
+            desti.Add("Calbayog");
+            desti.Add("Camiguin");
+            desti.Add("Cauayan");
+
+            Console.WriteLine("Flight Management\n");
+
+            Console.WriteLine("Locations \nManila   |       ");
+
+            Console.WriteLine("Enter Origin Place: ");
+            string origininput = Console.ReadLine();
+
+            Console.WriteLine("Enter Destination Place: ");
+            string destiInput = Console.ReadLine();
         }
     }
 }
